ec0845a90b62bcd4e1290d4c10b63e853f252494
hamletfg <hamletfg@gmail.com>
2025-03-02 15:39:46 -0700

    add event listener and read input functions

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,3 +1,28 @@
+/* select html elements */
 const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
+
+/* array to store todos */
+let allTodos = [];
+
+/* event listener to grab inputed text in form */
+todoForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  addTodo();
+})
+
+/* function to read text from input field and add to array */
+function addTodo(){
+  const todoText = todoInput.value.trim();
+  if(todoText.length > 0){  /* code will be only executed if a user enters text */
+    allTodos.push(todoText);
+    createTodoItem(todoText);
+    todoInput.value = "";
+  }
+}
+
+function createTodoItem(todo){
+  const todoLI = document.createElement("li");
+  
+}
