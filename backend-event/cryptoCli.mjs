03356ac6d40df92427a2aeb64068e9aee0635430
hamletfg <hamletfg@gmail.com>
2025-04-29 23:29:27 -0600

    Add encryption function

--- backend-event/cryptoCli.mjs
+++ backend-event/cryptoCli.mjs
@@ -5,3 +5,20 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// Shared crypto setup
+const algorithm = 'aes-256-cbc';
+const password = 'ILov3Mt3c!';
+const key = crypto.scryptSync(password, 'salt', 32);
+const iv = Buffer.alloc(16, 0); // Initialization vector
+
+// Encrypt function
+const encryptFile = (inputPath, outputPath) => {
+  const readStream = fs.createReadStream(inputPath);
+  const writeStream = fs.createWriteStream(outputPath);
+  const cipher = crypto.createCivpheriv(algorithm, key, iv);
+  readStream.pipe(cipher).pipe(writeStream);
+  writeStream.on('finish', () => {
+    console.log('✅ File encrypted successfully.');
+  });
+};
